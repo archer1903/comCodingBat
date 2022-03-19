ea25cdeb611e8193591a2087e59cb350cbf47eef
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-03-19 15:42:15 +0300

    string2 added

---
+++ src/string2/E01_doubleChar.java
@@ -0,0 +1,25 @@
+package string2;
+
+public class E01_doubleChar {
+
+    //Given a string, return a string where for every
+    // char in the original, there are two chars.
+    //doubleChar("The") → "TThhee"
+    public static void main(String[] args) {
+
+        System.out.println(doubleChar("The"));
+    }
+    public static String doubleChar(String str) {
+
+        String str2="";
+
+        for(int i=0; i<str.length(); i++) {
+            str2+=str.substring(i,i+1);
+            str2+=str.substring(i,i+1);
+        }
+
+        return str2;
+
+    }
+
+}
