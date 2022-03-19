6057f5a56883519d96a52fea3502c75e8c7e46d0
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-03-19 15:13:31 +0300

    logic2 examples.

--- src/logic2/E02_makeBricks.java
+++ src/logic2/E02_makeBricks.java
@@ -2,6 +2,12 @@ package logic2;
 
 public class E02_makeBricks {
     public static void main(String[] args) {
+//We want to make a row of bricks that is goal inches long.
+// We have a number of small bricks (1 inch each) and
+// big bricks (5 inches each). Return true if it is possible
+// to make the goal by choosing from the given bricks.
+// This is a little harder than it looks and can be done
+// without any loops.
 
        boolean result= makeBricks(2, 1000000, 100003);
         System.out.println(result);
