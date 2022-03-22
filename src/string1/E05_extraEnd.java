59c5ce47dbc7d5640f3e982410afa7ecff79e152
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-03-22 14:40:42 +0300

    warmup2 E06 added

---
+++ src/string1/E05_extraEnd.java
@@ -0,0 +1,28 @@
+package string1;
+
+public class E05_extraEnd {
+    public static void main(String[] args) {
+
+/*
+Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+The string length will be at least 2.
+
+extraEnd("Hello") → "lololo"
+extraEnd("ab") → "ababab"
+extraEnd("Hi") → "HiHiHi"
+*/
+
+        System.out.println( extraEnd("Hi"));
+    }
+
+    private static String extraEnd(String str) {
+
+        if(str.length()>=2)
+        return str.substring(str.length()-2).repeat(3);
+
+        return "null";
+
+    }
+
+
+}
