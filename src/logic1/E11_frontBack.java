94ca83a1275435f5536910db07748d3eeaaf84b0
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-03-24 12:28:11 +0300

    Logic 01 E11 added.

---
+++ src/logic1/E11_frontBack.java
@@ -0,0 +1,30 @@
+package logic1;
+
+public class E11_frontBack {
+    public static void main(String[] args) {
+
+/*        Given a string, return a new string where the first and last chars have been exchanged.
+
+        frontBack("code") → "eodc"
+        frontBack("a") → "a"
+        frontBack("ab") → "ba"*/
+
+        System.out.println(frontBack("eodc"));
+    }
+    public static String frontBack(String str) {
+        String str2="";
+        if(!str.isEmpty()){
+            String[] arr=str.split("");
+            String fLet=arr[0];
+            String lLet=arr[str.length()-1];
+            String temp=arr[0];
+            arr[0]=lLet;
+            arr[str.length()-1]=temp;
+            for(String harf:arr){
+                str2+=harf;
+            }
+            return str2;
+        }
+        return str2;
+    }
+}
