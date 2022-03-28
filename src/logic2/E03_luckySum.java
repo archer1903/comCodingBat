c4de2243519593233d359611b8450ca02054ae0e
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-03-28 21:37:14 +0300

    logic2 E03_luckySum added

---
+++ src/logic2/E03_luckySum.java
@@ -0,0 +1,28 @@
+package logic2;
+
+public class E03_luckySum {
+    public static void main(String[] args) {
+/*
+        Given 3 int values, a b c, return their sum.
+        However, if one of the values is 13
+        then it does not count towards the sum and
+        values to its right do not count. So for example,
+        if b is 13, then both b and c do not count.
+
+
+        luckySum(1, 2, 3) → 6
+        luckySum(1, 2, 13) → 3
+        luckySum(1, 13, 3) → 1*/
+        System.out.println(luckySum(1, 2, 3));
+    }
+
+    public static int luckySum(int i, int i1, int i2) {
+
+        if(i==13) return 0;
+        if(i1==13) return i;
+        if(i2==13) return i+i1;
+        else return i+i1+i2;
+
+
+    }
+}
