5749130dd9d0c2027d0b075442d76ba87461828e
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-06 23:56:41 +0300

    warmup1 E14_or35 added

---
+++ src/warmup1/E14_or35.java
@@ -0,0 +1,20 @@
+package warmup1;
+
+public class E14_or35 {
+
+/*    Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+
+
+    or35(3) → true
+    or35(10) → true
+    or35(8) → false*/
+public static void main(String[] args) {
+    System.out.println(or35(3));
+}
+    public static boolean or35(int n) {
+
+        if(n%3==0||n%5==0) return true;
+        return false;
+    }
+
+}
