e2f72e2bf2aa643e12fe4a27efada05d33675f58
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-14 17:42:46 +0300

    C24_intMax

---
+++ src/warmup1/C24_intMax.java
@@ -0,0 +1,21 @@
+package warmup1;
+
+public class C24_intMax {
+    public static void main(String[] args) {
+        /*
+Given three int values, a b c, return the largest.
+
+
+intMax(1, 2, 3) → 3
+intMax(1, 3, 2) → 3
+intMax(3, 2, 1) → 3
+*/
+        System.out.println(intMax(1,2,3));
+    }
+    public static int intMax(int a, int b, int c) {
+        if(a>b&&a>c) return a;
+        if(b>a&&b>c) return b;
+        return c;
+    }
+
+}
