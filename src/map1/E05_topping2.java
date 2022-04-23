d3cdeb83f76dfe1a50f217a5a56f02629ef8f9f5
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-23 11:32:53 +0300

    E05_topping2

---
+++ src/map1/E05_topping2.java
@@ -0,0 +1,35 @@
+package map1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class E05_topping2 {/*Given a map of food keys and their topping values, modify and return the map as follows: if the key "ice cream" has a value, set that as the value for the key "yogurt" also. If the key "spinach" has a value, change that value to "nuts".
+
+
+topping2({"ice cream": "cherry"}) → {"yogurt": "cherry", "ice cream": "cherry"}
+topping2({"spinach": "dirt", "ice cream": "cherry"}) → {"yogurt": "cherry", "spinach": "nuts", "ice cream": "cherry"}
+topping2({"yogurt": "salt"}) → {"yogurt": "salt"}
+*/
+
+    public static void main(String[] args) {
+
+        Map<String, String>myMap=new HashMap<>();
+        myMap.put("ice cream","cherry");
+
+        System.out.println(topping2(myMap));
+    }
+    public static Map<String, String> topping2(Map<String, String> map) {
+
+        if(map.containsKey("ice cream")){
+            String temp=map.get("ice cream");
+            map.put("yogurt",temp);
+        }
+        if(map.containsKey("spinach")){
+            map.put("spinach","nuts");
+        }
+
+        return map;
+    }
+
+
+}
