55f5aa962c93d4fd5f38e23a55eed5a5af6dd0d3
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-28 16:42:30 +0300

    E04_copies3

---
+++ src/functional1/E04_copies3.java
@@ -0,0 +1,30 @@
+package functional1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class E04_copies3 {/*Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together.
+
+
+copies3(["a", "bb", "ccc"]) → ["aaa", "bbbbbb", "ccccccccc"]
+copies3(["24", "a", ""]) → ["242424", "aaa", ""]
+copies3(["hello", "there"]) → ["hellohellohello", "theretherethere"]
+*/
+
+    public static void main(String[] args) {
+
+        List<String> str = new ArrayList<>(Arrays.asList("a","bb","ccc"));
+
+        System.out.println(copies3(str));
+
+
+    }
+    public static List<String> copies3(List<String> strings) {
+
+        strings.replaceAll(s->s.repeat(3));
+        return strings;
+
+    }
+
+}
