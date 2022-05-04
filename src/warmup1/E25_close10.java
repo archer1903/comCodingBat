864fc4ee8813c3f5a76c7ac4ceccd8c749c2b933
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-05-04 17:05:12 +0300

    E25_close10

--- src/warmup1/E25_close10.java
+++ src/warmup1/E25_close10.java
@@ -1,6 +1,6 @@
 package warmup1;
 
-public class C25_close10 {
+public class E25_close10 {
     /*Given 2 int values, return whichever value is nearest to the value 10, or return 0 in the event of a tie. Note that Math.abs(n) returns the absolute value of a number.
 
 
