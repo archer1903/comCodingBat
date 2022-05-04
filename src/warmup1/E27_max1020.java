99d021b579c9f602a9e26f219f7111b97b88ccfa
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-05-04 17:05:42 +0300

    E27_max1020

--- src/warmup1/E27_max1020.java
+++ src/warmup1/E27_max1020.java
@@ -1,6 +1,6 @@
 package warmup1;
 
-public class C27_max1020 {
+public class E27_max1020 {
     /*
 Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if neither is in that range.
 
