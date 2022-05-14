327a73f628018ed9cda40106bac06e8b3ad3c106
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-05-14 12:24:48 +0300

    E03_makePi

---
+++ src/array1/E03_makePi.java
@@ -0,0 +1,24 @@
+package array1;
+
+import java.util.Arrays;
+
+public class E03_makePi {
+    /*
+
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+makePi() → [3, 1, 4]
+*/
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(makePi()) );
+
+    }
+    public static int[] makePi() {
+        int[] arr={3,1,4};
+        return arr;
+    }
+
+
+}
