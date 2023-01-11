e190633ffddc080aa57abf47e119382dee87487d
hkartal <huseyin86kartal@gmail.com>
2023-01-11 22:31:02 +0300

    Array-1 > start1

---
+++ src/array1/E17_start1.java
@@ -0,0 +1,27 @@
+package array1;
+
+public class E17_start1 {
+
+/*
+Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+
+
+start1([1, 2, 3], [1, 3]) → 2
+start1([7, 2, 3], [1]) → 1
+start1([1, 2], []) → 1*/
+    public static void main(String[] args) {
+        int a[]={1, 2, 3};
+        int b[]={1, 3};
+
+        System.out.println(start1(a, b));
+
+    }
+
+    public static int start1(int[] a, int[] b) {
+        int count=0;
+        if(a.length>0&& a[0]==1)count++;
+        if(b.length>0&& b[0]==1)count++;
+        return count;
+    }
+
+}
